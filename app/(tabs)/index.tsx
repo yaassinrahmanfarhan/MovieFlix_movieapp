3945f269204f15976c2d686ecddc4e77d7bb7dac
YR <yaassinrahmanfarhan@gmail.com>
2026-05-21 00:53:10 +0600

    api ts created

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,12 @@ import { Text, View,Image, ScrollView } from "react-native";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
+import {useRouter} from "expo-router";
+
 
 export default function Index() {
+  const router = useRouter();
+
   return (
     <View className = 'flex-1 bg-primary'>
       <Image source={images.bg} className = "absolute w-full z-0" />
@@ -12,7 +16,10 @@ export default function Index() {
       contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
         <View className="flex-1 mt-5">
-          <SearchBar />
+          <SearchBar 
+            onPress={() => router.push("/search")}
+            placeholders = "Search for a Movie"
+          />
         </View>
       </ScrollView>
     </View>
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,14 +1,18 @@
 import {Text, View, Image, TextInput } from "react-native";
 import React from "react";
 import { icons } from "@/constants/icons";
+interface Props{
+    placeholders: string;
+    onPress?: () => void;
+}
 
-const SearchBar = () => {
+const SearchBar = ({placeholders, onPress}: Props) => {
     return (
         <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
                 <Image source = {icons.search} className = "size-5" resizeMode = "contain" tintColor='#ab8bff'/>
                 <TextInput 
-                onPress={() => console.log("Search")}
-                placeholder="Search"
+                onPress={(onPress)}
+                placeholder={placeholders}
                 value=""
                 onChange={() => {}}
                 placeholderTextColor= "#a8b5db"
